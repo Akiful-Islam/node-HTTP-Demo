8739686923dcf18aa788f2ccd3ead8747e441e46
Arian Islam <arian.islam024@gmail.com>
2023-02-28 12:16:00 +0600

    Error Handling added

--- server/index.ts
+++ server/index.ts
@@ -4,29 +4,39 @@ import * as path from "path";
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const filePath = "../public" + req.url; // Convert the requested URL to a file path
-    const fileExists = fs.existsSync(filePath); // Check if the file exists
+    const filePath = "../public" + req.url;
+    const fileExists = fs.existsSync(filePath);
+
     if (fileExists) {
-      const fileStream = fs.createReadStream(filePath);
       const stat = fs.statSync(filePath);
-      const contentType = getContentType(filePath);
-      const contentLength = stat.size.toString();
-      const contentSize = getFileSize(stat.size);
-      logRequest(req, res.statusCode, contentLength, contentSize, contentType);
-      res.setHeader("Content-Type", contentType);
-      res.setHeader("Content-Length", contentLength);
-      res.setHeader("Content-File-Size", contentSize);
-      fileStream.pipe(res); // Send the file contents to the browser
+
+      if (stat.isDirectory()) {
+        res.statusCode = 403;
+        res.end("Forbidden");
+        logRequest(req, res);
+      } else {
+        const fileStream = fs.createReadStream(filePath);
+        const contentType = getContentType(filePath);
+        const contentLength = stat.size.toString();
+        const contentSize = getFileSize(stat.size);
+
+        logRequest(req, res, contentLength, contentSize, contentType);
+
+        res.setHeader("Content-Type", contentType);
+        res.setHeader("Content-Length", contentLength);
+        res.setHeader("Content-File-Size", contentSize);
+        fileStream.pipe(res);
+      }
     } else {
-      const errorCode = 404;
-      res.statusCode = errorCode;
+      res.statusCode = 404;
       res.end("File not found");
-      logRequest(req, errorCode);
+
+      logRequest(req, res);
     }
   }
 );
 
-function getContentType(filePath: string): string {
+const getContentType = (filePath: string): string => {
   const extname = path.extname(filePath);
   switch (extname) {
     case ".html":
@@ -46,40 +56,44 @@ function getContentType(filePath: string): string {
     default:
       return "application/octet-stream";
   }
-}
+};
 
-function getFileSize(bytes: number): string {
+const getFileSize = (bytes: number): string => {
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
   if (bytes == 0) {
     return "0 Byte";
   }
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + " " + sizes[i];
-}
+};
 
-function logRequest(
+const logRequest = (
   req: http.IncomingMessage,
-  statusCode: number,
+  res: http.ServerResponse,
   contentLength?: string,
   contentSize?: string,
   contentType?: string
-) {
-  let logMessage: string;
+) => {
+  let logMessage = `Request: ${req.method} Status code: ${res.statusCode} URL: ${req.url}`;
+
   if (!contentLength || !contentSize || !contentType) {
-    logMessage = `Request: ${req.method} url: ${
-      req.url
-    } Status code: ${statusCode}\n File Not Found.\n Request Made at ${new Date().toISOString()}\n`;
+    if (res.statusCode === 403) {
+      logMessage += `\nForbidden Access\n`;
+    } else if (res.statusCode === 404) {
+      logMessage += `\nFile not Found\n`;
+    }
   } else {
-    logMessage = `Request: ${req.method} url: "${req.url}" path: "${
-      "../public" + req.url
-    }" Status code: ${statusCode}\n File Type: ${contentType} File Length: ${contentLength}bytes File Size: ${contentSize}\n Request Made at ${new Date().toISOString()}\n`;
+    logMessage += `\nType: ${contentType} - Length: ${contentLength}bytes - Size: ${contentSize}\n`;
   }
+
+  logMessage += `Time: ${new Date().toISOString()}\n`;
+
   fs.appendFile("log.txt", logMessage + "\n", (err) => {
     if (err) {
       console.error(`Error writing to log file: ${err.message}`);
     }
   });
-}
+};
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
