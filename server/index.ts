9e44cc569f7192062c77f0554411ef7d2c0e6a6a
Arian Islam <arian.islam024@gmail.com>
2023-03-05 13:20:55 +0600

    async await promise added

--- server/index.ts
+++ server/index.ts
@@ -4,48 +4,44 @@ import { getContentType, getFileSize } from "./headerMethods";
 import logRequest from "./logRequest";
 
 const server = http.createServer(
-  (req: http.IncomingMessage, res: http.ServerResponse) => {
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const filePath = "../public" + req.url;
     try {
-      const stat = fs.statSync(filePath);
+      await fs.promises.access(filePath, fs.constants.R_OK);
+      const stat = await fs.promises.stat(filePath);
 
       if (stat.isDirectory()) throw new Error("URL is a directory");
-
-      fs.accessSync(filePath, fs.constants.R_OK);
-
       const fileStream = fs.createReadStream(filePath);
 
       const length = stat.size.toString();
       const size = getFileSize(stat.size);
       const type = getContentType(filePath);
 
-      logRequest(req, res, { length, size, type });
-
       res.setHeader("Content-Type", type);
       res.setHeader("Content-Length", length);
-
       fileStream.pipe(res);
+      logRequest(req, res, { length, size, type });
     } catch (error) {
       const err = error as NodeJS.ErrnoException;
 
-      if (err.code === "ENOENT") {
-        const error = new Error("File not found");
+      if (err.message === "URL is a directory") {
+        const error = new Error("URL is a directory.");
 
         res.statusCode = 404;
         res.end(error.message);
 
         logRequest(req, res, error);
-      } else if (err.message === "URL is a directory") {
-        const error = new Error("Bad Request. File is a directory.");
+      } else if (err.code === "ENOENT") {
+        const error = new Error("File not found.");
 
-        res.statusCode = 400;
+        res.statusCode = 404;
         res.end(error.message);
 
         logRequest(req, res, error);
       } else if (err.code === "EACCES") {
-        const error = new Error("Forbidden. File Access denied.");
+        const error = new Error("Unauthorized. File Access denied.");
 
-        res.statusCode = 403;
+        res.statusCode = 401;
         res.end(error.message);
 
         logRequest(req, res, error);
